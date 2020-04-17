124ebdc5ed414153f0348999446e03c94c1da0ec
lambSauce9 <45115684+lambSauce9@users.noreply.github.com>
2020-04-17 18:48:37 +0700

    BLUE SCREEN

---
+++ display.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<SDL2/SDL.h>
+#include<string>
+
+class Display
+{
+public:
+	Display(int width, int height, const std::string& title);
+
+	void Clear(float r, float g, float b, float a);
+	void Update();
+	bool IsClosed();
+
+	virtual ~Display();
+
+protected:
+private:
+	Display(const Display& other) {}
+	void operator=(const Display& other) {}
+
+	SDL_Window* m_window;
+	SDL_GLContext m_glContext;
+	bool m_isClosed;
+};
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<GL/glew.h>
+#include"display.h"
+
+int main(int argc, char* argv[])
+{
+	Display display(800, 600, "Hello World");
+
+	while (!display.IsClosed())
+	{
+		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
+
+		display.Update();
+	}
+
+	return 0;
+}
